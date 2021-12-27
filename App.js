a225711492fbd155da63194f58fe1118b7355f72
ismauy <isma.ferrazzi@gmail.com>
2021-12-27 22:42:49 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, TextInput, Text, Button, Alert, ScrollView } from 'react-native';
+import Start from './components/Start';
+import Chat from './components/Chat';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+export default class HelloWorld extends Component {
+
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Start"
+        >
+          <Stack.Screen
+            name="Start"
+            component={Start}
+          />
+          <Stack.Screen
+            name="Chat"
+            component={Chat}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
+
+
