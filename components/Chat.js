d8d19a3b2caff5d7890338aa1ef1ce6b7d36f2a1
ismauy <isma.ferrazzi@gmail.com>
2021-12-29 21:48:13 +0100

    chat screen

--- components/Chat.js
+++ components/Chat.js
@@ -1,18 +1,98 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Platform, KeyboardAvoidingView } from 'react-native';
+import { Bubble, GiftedChat } from 'react-native-gifted-chat';
 
-export default class Chat extends React.Component {
+
+export default class Chat extends Component {
+    constructor() {
+        super();
+        this.state = {
+            messages: [],
+        };
+    }
+
+    componentDidMount() {
+        this.setState({
+            messages: [
+                {
+                    _id: 1,
+                    text: `Hello ${this.props.route.params.name}`,
+                    createdAt: new Date(),
+                    user: {
+                        _id: 2,
+                        name: 'React Natvie',
+                        avatar: 'https://placeimg.com/140/140/any',
+                    },
+                },
+                {
+                    _id: 2,
+                    text: `${this.props.route.params.name} joined the chat. `,
+                    createdAt: new Date(),
+                    system: true,
+                },
+            ],
+        });
+    }
+
+    onSend(messages = []) {
+        this.setState(previousState => ({
+            messages: GiftedChat.append(previousState.messages, messages),
+        }));
+    }
+
+    renderBubble(props) {
+        return (
+            <Bubble
+                {...props}
+                wrapperStyle={{
+                    right: {
+                        backgroundColor: '#000'
+                    }
+                }}
+            />
+        )
+    }
+
 
     render() {
         let name = this.props.route.params.name;
-
-
         this.props.navigation.setOptions({ title: name });
 
+        let bgColor = this.props.route.params.bgColor;
+
         return (
-            <View>
-                {/* Rest of the UI */}
+            <View style={styles.container}>
+                <View
+                    style={{
+                        backgroundColor: bgColor,
+                        width: '100%',
+                        height: '100%',
+                    }}
+                >
+                    <GiftedChat
+                        style={styles.giftedChat}
+                        renderBubble={this.renderBubble.bind(this)}
+                        messages={this.state.messages}
+                        onSend={messages => this.onSend(messages)}
+                        user={{ _id: 1 }}
+                    />
+                    {Platform.OS === 'android' ? (
+                        <KeyboardAvoidingView behavior="height" />
+                    ) : null}
+                </View>
             </View>
         );
-    };
+    }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    giftedChat: {
+        color: '#000',
+    },
+});
