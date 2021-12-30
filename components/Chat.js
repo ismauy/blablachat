ce2122042e8460ce7bd491b065d732d2bd42e092
ismauy <isma.ferrazzi@gmail.com>
2021-12-30 20:16:35 +0100

    start.js update

--- components/Chat.js
+++ components/Chat.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import PropTypes from 'prop-types';
 
 export default class Chat extends React.Component {
 
+    componentDidMount() {
+        this.props.navigation.setOptions({ title: this.props.route.params.name });
+    }
+
     render() {
-        let name = this.props.route.params.name;
-
-
-        this.props.navigation.setOptions({ title: name });
+        const { name, bgColor } = this.props.route.params;
 
         return (
-            <View>
+            <View style={{
+                backgroundColor: bgColor,
+                width: '100%',
+                height: '100%'
+            }}>
                 {/* Rest of the UI */}
             </View>
         );
     };
+}
+
+Chat.propTypes = {
+    navigation: PropTypes.shape({
+        navigate: PropTypes.func.isRequired,
+    }).isRequired
 }
